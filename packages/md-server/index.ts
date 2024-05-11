22230c69fc9908a11967ab22a34be8c2bf542804
aho4ahoaho <aho4ahoaho@outlook.jp>
2024-05-11 12:04:32 +0900

    環境構築

--- packages/md-server/index.ts
+++ packages/md-server/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import Express from "express";
+import path from "path";
+const app = Express();
+
+
+//クライアントのファイルを配信する
+app.use(Express.static(path.join(__dirname, "client")))
+
+//JSONをパース出来るようにする
+app.use(Express.json());
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello, World!" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
